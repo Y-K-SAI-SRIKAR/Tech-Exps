fe2a1c5ea7dd87eeb4d7d2a124792bbd0dc6c1d5
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2026-09-17 23:27:51 +0530

    Experimented with AWS BEDROCK

---
+++ LLM Exps/Exp With BedRock AWS/main.py
@@ -0,0 +1,4 @@
+from models.model import main
+
+response = main()
+print(response)
---
+++ LLM Exps/Exp With BedRock AWS/models/model.py
@@ -0,0 +1,32 @@
+import json
+import os
+import boto3
+from dotenv import load_dotenv 
+
+load_dotenv()
+def main():
+    prompt_Content = str(input("Enter your Prompt:"))
+    client = boto3.client("bedrock-runtime", region_name="us-east-1")
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "text": prompt_Content
+                }
+            ]
+        }
+    ]
+    model_id = os.getenv("MODEL_ID")
+    response = client.converse(
+        modelId=model_id,
+        messages=messages,
+        inferenceConfig={
+            "maxTokens": 512,
+            "temperature": 0.75
+        }
+    )
+
+    response_text = response["output"]["message"]["content"][0]["text"]
+    print("Model response to Prompt: \n")
+    return response_text
